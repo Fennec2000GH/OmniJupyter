3c58b6316a07ba463734b4ec52949e74efda3547
root <root@DESKTOP-1N126KV.localdomain>
2021-05-09 13:07:44 -0400

    webpage saving files with correct extension working

--- src/website/index.js
+++ src/website/index.js
@@ -1,6 +1,7 @@
 
-const fs = require('fs');
+// const fs = require('fs');
 
+var lang = "Python3";
 
 function runCell() {
     var runBtn = document.activeElement;
@@ -10,22 +11,46 @@ function runCell() {
     // runBtn.innerText = code;
     // try { fs.writeFile('./file.txt', 'sample text'); } 
     // catch (error) { console.log(error); }
-const FileSaver = require('file-saver');
-    var filename = 'sample.txt';
-    var file = new Blob([code], {type: 'text/plain'});
-    FileSaver.saveAs(file, filename);
 
-    // if (window.navigator.msSaveOrOpenBlob) // IE10+
-    //     window.navigator.msSaveOrOpenBlob(file, filename);
-    // else { // Others
-    //     var a = document.createElement("a"),
-    //             url = URL.createObjectURL(file);
-    //     a.href = url;
-    //     a.download = filename;
-    //     document.body.appendChild(a);
-    //     a.click();
-    //     setTimeout(function() {
-    //         document.body.removeChild(a);
-    //         window.URL.revokeObjectURL(url);  
-    //     }, 0); }
+    var ext;
+    switch(lang) {
+        case "Python3":
+            ext = "py";
+            break;
+        case "R":
+            ext = "r";
+            break;
+        case "Julia":
+            ext = "jl";
+            break;
+        case "Rust":
+            ext = "rs";
+            break;
+        default:
+            ext = "sh";
+    }
+
+    var filename = 'sample.' + ext;
+    var file = new Blob([code], {type: 'text/plain'});
+    // FileSaver.saveAs(file, filename);
+
+    if (window.navigator.msSaveOrOpenBlob) // IE10+
+        window.navigator.msSaveOrOpenBlob(file, filename);
+    else { // Others
+        var a = document.createElement("a"),
+                url = URL.createObjectURL(file);
+        a.href = url;
+        a.download = filename;
+        document.body.appendChild(a);
+        a.click();
+        setTimeout(function() {
+            document.body.removeChild(a);
+            window.URL.revokeObjectURL(url);  
+        }, 0); }
+}
+
+function langFunction() {
+    var cell = document.activeElement.parentElement;
+    lang = cell.querySelector("select").value;
+    console.log(lang);
 }
