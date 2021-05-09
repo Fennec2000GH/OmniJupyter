2fb5accd59e6afcc75862069d675ce26cbe0c254
root <root@DESKTOP-1N126KV.localdomain>
2021-05-09 15:51:11 -0400

    pickled file successfully created from document tree

--- src/website/doctree.py
+++ src/website/doctree.py
@@ -1,17 +1,15 @@
 
-import numpy as np, sys
+import io, numpy as np, os, pickle, sys, warnings
+from contextlib import redirect_stdout
 from collections import OrderedDict
 from jina import Document
 
-
-# ensuring all files in dependency link
 # '|' separates filenames into dependency levels, with first file after '|' being the immediate parent of all other files before the
 # next occurrence of '|'
 filenames = sys.argv[1:]
-ext = filenames[0].split('.')[-1]
-assert all([fname.endswith(ext) or fname == '|' for fname in filenames])
+assert all([os.path.exists(path=fname) or fname == '|' for fname in filenames])
 
-curr_head = filenames[0]  # current head of immediate dependency group of docs
+curr_head = None  # current head of immediate dependency group of docs
 doc_dict = OrderedDict()
 
 for fname in filenames:
@@ -27,14 +25,30 @@ for fname in filenames:
             raise ValueError(ofcorrecttype='Current document as head of immediate group already exists.')
         curr_head = fname
         with open(file=curr_head, mode='r') as f:
-            doc_Dict[curr_head] = Document(content=f)
+            doc_dict[curr_head] = Document(content=f.read())
         continue
 
     # appending docs to current head doc
     with open(file=fname, mode='r') as f:
-        curr_doc = Document(content=f)
+        curr_doc = Document(content=f.read())
         doc_dict[curr_head].chunks.append(curr_doc)
         doc_dict[fname] = curr_doc
 
-doctree_plot = doc_dict[filenames[0]].plot()
-print(type(doctree_plot))
+with open(file='doctree.pkl', mode='wb') as f:
+    pickle.dump(obj=doc_dict, file=f)
+
+# str_io = io.StringIO()
+# with redirect_stdout(new_target=str_io):
+#     doc_dict[filenames[0]].plot()
+# out = str_io.getvalue()
+
+# print('-' * 100)
+# print(out)
+
+# with warnings.catch_warnings(record=True) as w:
+#     warnings.simplefilter("always")
+#     doc_dict[filenames[0]].plot()
+#     print('-' * 100)
+#     print(w)
+
+doc_dict[filenames[0]].plot()
